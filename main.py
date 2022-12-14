ce0f141c14fde86729e0542b4b1c6c52c22c3c20
lucasaclaro <102222870+lucasaclaro@users.noreply.github.com>
2022-12-14 17:02:44 -0300

    commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data_base = pd.read_csv('data/municipio.csv')
+
+#print(data_base.head())
+
+selecao = data_base['id_municipio'] == 1100015
+
+araraquara = data_base.query('id_municipio == 3503208')
+sao_carlos = data_base.query('id_municipio == 3548906')
+
+plt.style.use('seaborn-darkgrid')
+
+plt.figure(figsize=(16, 7))
+
+
+plt.title('Comparação PIB - Araraquara/SP e São Carlos/SP', loc='left', fontsize=18, fontweight=200)
+plt.xlabel('Período')
+plt.ylabel('Valor (R$ Bi)')
+sns.lineplot(araraquara, x='ano', y='pib', color='#008c4a', linewidth=4, alpha=0.7)
+sns.lineplot(sao_carlos, x='ano', y='pib', color='#000099', linewidth=4, alpha=0.7)
+plt.show()
+
+print(araraquara)
+
